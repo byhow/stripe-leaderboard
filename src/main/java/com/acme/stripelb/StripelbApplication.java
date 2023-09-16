65358703629a979c7519376586a0746c241e2a17
Baihao <by.how@hotmal.com>
2023-09-15 17:24:17 -0700

    feat: init spring

---
+++ src/main/java/com/acme/stripelb/StripelbApplication.java
@@ -0,0 +1,13 @@
+package com.acme.stripelb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripelbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripelbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/stripelb/StripelbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.stripelb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripelbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
